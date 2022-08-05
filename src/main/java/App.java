840002fc4c4a9ccfeac86048315a3aa9d81d2230
Kyril <moreunderground@gmail.com>
2022-08-06 00:08:16 +0300

    my commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        //System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean compareHelloWorlds = bean == bean2;
+        boolean compareCats = cat == cat2;
+        System.out.println(compareHelloWorlds);
+        System.out.print(compareCats);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    public String cat;
+
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
